6e2c698c919b7e71844622f7bee44c6966db6811
Samuel Girardin <samuelgirardin@pm.me>
2026-01-20 19:08:37 +0100

    update endpoint

--- server/api/agencies-home.ts
+++ server/api/agencies-home.ts
@@ -3,12 +3,10 @@ export default defineEventHandler(async (event) => {
   setHeader(event, "Cache-Control", "public, s-maxage=86400");
   console.log("Fetching agencies from backend");
 
-  return await $fetch(
-    `${config.public.apiBase}/public/agencies/?page_size=30`,
-    {
-      //   headers: {
-      //     Authorization: `Token ${config.apiToken}`,
-      //   },
+  return await $fetch(`${config.public.apiBase}/agencies/?page_size=30`, {
+    headers: {
+      // Authorization: `Token ${config.apiToken}`,
+      Accept: "application/json",
     },
-  );
+  });
 });
