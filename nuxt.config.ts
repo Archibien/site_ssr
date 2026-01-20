80f2a9ab78347444b579cc02f1a14277f5491e0b
Samuel Girardin <samuelgirardin@pm.me>
2026-01-20 22:23:59 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
       apiBase: "",
     },
   },
+  nitro: {
+    preset: "vercel",
+  },
   routeRules: {
     "/": { isr: 86400 }, // homepage cached 24h
     "/agence/**": {
