fbe1d13dd39475c75109add9eb526a4f21cdec07
Samuel Girardin <samuelgirardin@pm.me>
2026-01-20 22:12:46 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   },
   routeRules: {
     "/": { isr: 86400 }, // homepage cached 24h
-    "/agency/**": {
+    "/agence/**": {
       isr: 86400, // regenerate at most once per 24h
     },
   },
